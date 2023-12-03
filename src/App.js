b019629aedc5139c9bc735044061cfa1db989a5a
“Abhishek <kabhi12132002@gmail.com>
2023-12-03 17:10:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloud, faCloudRain, faBolt, faSmog, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   const [timeOfDay, setTimeOfDay] = useState('day');
   const [weatherCondition, setWeatherCondition] = useState('clear');
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords;
-
-      const apiKey = 'ba17e4f50a4784643b4acb1d6bd07644';
-
-      const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
+    const fetchWeather = async () => {
       try {
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+
+        const { latitude, longitude } = position.coords;
+
+        const apiKey = 'ba17e4f50a4784643b4acb1d6bd07644';
+        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
         const response = await axios.get(weatherApiUrl);
         const weather = response.data.weather[0].main.toLowerCase();
         setWeatherCondition(weather);
-        console.log(weather)
 
         const currentHour = new Date().getHours();
         const isDayTime = currentHour >= 6 && currentHour < 18;
@@ -28,13 +32,48 @@ const App = () => {
         console.error('Error fetching weather data:', error);
         console.error('Response:', error.response);
       }
-    });
+    };
+
+    fetchWeather();
   }, []);
+
+  const getWeatherIcon = () => {
+    switch (weatherCondition) {
+      case 'clear':
+        return timeOfDay === 'day' ? faSun : faMoon;
+      case 'clouds':
+        return faCloud;
+      case 'rainy':
+        return faCloudRain;
+      case 'thunderstorms':
+        return faBolt;
+      case 'mist':
+        return faSmog;
+      default:
+        return faMoon;
+    }
+  };
+
+  const textColor = timeOfDay === 'night' ? '#fff' : '#333';
 
   return (
     <div className={`app ${timeOfDay} ${weatherCondition}`}>
-      <h1>Weather Based Theme Changer</h1>
-      {/* Your other components and content go here */}
+      <div className="weather-animation">
+        <div className="animation-container">
+          <FontAwesomeIcon icon={getWeatherIcon()} className="icon" />
+        </div>
+      </div>
+
+      
+      <div className="content" >
+        <h2 style={{ color: textColor }}>{`${timeOfDay}`}</h2>
+        <h1 style={{ color: textColor }}>Weather Based Theme Changer</h1>
+        {/* Additional components and content go here */}
+        <div>
+          <p style={{ color: textColor }}>This is additional content.</p>
+          {/* Add more components as needed */}
+        </div>
+      </div>
     </div>
   );
 };
